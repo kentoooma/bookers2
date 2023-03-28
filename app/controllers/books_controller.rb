6c87e2beb7b5b572b5c4cc2cf35aa3cccffa1652
kentoooma <124879423+kentoooma@users.noreply.github.com>
2023-03-28 19:13:44 +0000

    [Add]loginページの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
+    @books = Book.all
   end
 
   def show
+    @user = @book.user
   end
 
   def edit
   end
+  
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+
+  private
+  
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 end
