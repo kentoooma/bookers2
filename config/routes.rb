bc0600ef3b162042f2c59c1ba45e58a3c04f96c6
kentoooma <124879423+kentoooma@users.noreply.github.com>
2023-04-01 13:52:38 +0000

    [Finish]課題4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
-  
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
